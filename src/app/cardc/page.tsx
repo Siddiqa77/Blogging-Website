86e9d1765ef4a65289638c18decda2d10a185720
Siddiqa Badar <147896187+Siddiqa77@users.noreply.github.com>
2024-10-20 21:45:20 +0500

    page.tsx

--- src/app/cardc/page.tsx
+++ src/app/cardc/page.tsx
@@ -19,11 +19,11 @@ const Blog = () => {
       The right fashion for your lifestyle is to be healthy. Fashion is a big domain in which you occupy trends, clothes, shoes, bags, accessories, makeup, hairstyle, body posture, etc. Fashion has its own body language its displays your personality, 
       the way you walk and talk. It helps you to be fit and attractive at the same time. Following fashion, you also desire to have a healthy body that helps you to be fit and fine, by doing this you have a fresh lifestyle. The lifestyle that helps you to grow mentally and physically.
 
-These innovations are indicative of how technology is evolving at a rapid pace, with each breakthrough bringing us closer to a future driven by smarter, faster, and more connected systems. Whether it's improving consumer experiences or pushing the boundaries of
+These innovations are indicative of how technology is evolving at a rapid pace, with each breakthrough bringing us closer to a future driven by smarter, faster, and more connected systems. Whether it&apos; s improving consumer experiences or pushing the boundaries of
  what quantum and AI systems can achieve, October 2024 stands out as a period rich with technological progress.It is 21st-century style, trends and fashion dominate the world more than anything else and control not only the way how people dress but also trends in homeware design, makeup fashion, and overall attitudes. Nowadays, fashion is bold and daring, and this reflects a current generation that is not afraid to say what they think or wear what they want. Fashion is highly followed by youth.
 
-“Fashion is not something that exists in dresses only. Fashion is in the sky, in the street, fashion has to do with ideas, the way we live, what is happening.”
-― Coco Chanel
+Fashion is not something that exists in dresses only. Fashion is in the sky, in the street, fashion has to do with ideas, the way we live, what is happening.
+Coco Chanel
 
 The young generation is a new leaf that loves to develop their personality by accepting fashion. Fashion can change from one second to the next, but what never changes is its hold over society and its role in the modern world. Fashion is so important that whole magazines are dedicated to it, TV programs dedicate hours of transmission time to the subject, and people discuss it between their friends continually.
       </p>
